b5c2b3c4a1609e6cc876d3b1b287c24e35bbb244
weiyanshi <shiweiyan12138@gmail.com>
2024-10-10 13:13:47 +0700

    fix: update demo.py & plot position

--- MCGaze_demo/demo.py
+++ MCGaze_demo/demo.py
@@ -10,7 +10,7 @@ import os
 import matplotlib.pyplot as plt
 import json
 
-BASE_DIR = os.getenv('BASE_DIR')
+BASE_DIR = os.getenv('BASE_DIR', '/app/Desktop/Dataset/pcit1')
 VIDEO_NAME = os.path.basename(BASE_DIR)
 
 # In[2]:
@@ -153,9 +153,9 @@ for clip in video_clip_set:
             gaze_data[frame_id] = {}
         for j in range(person_num):
             gaze_data[frame_id][f'person_{j}'] = {
-                'gaze': clip['gaze_p' + str(j)][i].tolist(),
-                # 'gaze': clip['gaze_p'+str(j)][i][0],
-                'head_bbox': [int(coord) for coord in clip['p' + str(j)][i]]
+                # 'gaze': clip['gaze_p' + str(j)][i].tolist(),
+                'gaze': clip['gaze_p'+str(j)][i][0].tolist(),
+                'head_bbox': clip['p'+str(j)][i],
             }
 
 
